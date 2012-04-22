cfdf794385f675818932f1d0bae68930a56851c6
Scott Pullen <s.pullen05@gmail.com>
2012-04-22 16:40:11 -0400

    removing rogue <

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
       <a><strong>Copyright 2011 <?php bloginfo('name'); ?></strong></a>
     </div>
     <div class="rss">
-      <a href="<?php bloginfo('rss2_url'); ?>">Latest Stories RSS</a> | <a href="<?php comments_rss_link('comment feed'); ?>">Comments RSS</a><
+      <a href="<?php bloginfo('rss2_url'); ?>">Latest Stories RSS</a> | <a href="<?php comments_rss_link('comment feed'); ?>">Comments RSS</a>
     </div>
   </div><!-- end footer -->
 </div><!-- end wrapper -->
