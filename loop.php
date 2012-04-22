0428735d7a538da5350cce3bc1cae111933bf04d
Scott Pullen <s.pullen05@gmail.com>
2012-04-22 16:39:04 -0400

    removing posts_pagination partial, guess it doesn't work

--- loop.php
+++ loop.php
@@ -1,4 +1,9 @@
-<?php get_posts_pagination(); ?>
+<?php if ( $wp_query->max_num_pages > 1 ) : ?>
+<div class="posts-navigation">
+  <div class="nav-previous"><?php next_posts_link( __( '<span class="posts-nav">&larr;</span> Older posts', 'spullen' ) ); ?></div>
+  <div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="posts-nav">&rarr;</span>', 'spullen' ) ); ?></div>
+</div>
+<?php endif; ?>
 
 <?php while ( have_posts() ) : the_post(); ?>
   <div class="post" id="post-<?php the_ID(); ?>">
@@ -6,4 +11,9 @@
   </div>
 <?php endwhile; ?>
 
-<?php get_posts_pagination(); ?>
+<?php if ( $wp_query->max_num_pages > 1 ) : ?>
+<div class="posts-navigation">
+  <div class="nav-previous"><?php next_posts_link( __( '<span class="posts-nav">&larr;</span> Older posts', 'spullen' ) ); ?></div>
+  <div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="posts-nav">&rarr;</span>', 'spullen' ) ); ?></div>
+</div>
+<?php endif; ?>
