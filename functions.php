96cf127cacdb0b7f574f9f7a5a4233441a280f68
Scott Pullen <s.pullen05@gmail.com>
2012-04-23 22:09:45 -0400

    adding html template

--- functions.php
+++ functions.php
@@ -1,7 +1,18 @@
 <?php
 
+add_action( 'init', 'register_my_menu' );
+ 
+//Register area for custom menu
+function register_my_menu() {
+    register_nav_menu( 'primary-menu', __( 'Primary Menu' ) );
+}
+
 //Some simple code for our widget-enabled sidebar
 if ( function_exists('register_sidebar') )
     register_sidebar();
+
+// Enable post thumbnails
+add_theme_support('post-thumbnails');
+set_post_thumbnail_size(250, 250, true);
 
 ?>
--- loop.php
+++ loop.php
@@ -1,15 +1,22 @@
-<?php if ( $wp_query->max_num_pages > 1 ) : ?>
-<div class="posts-navigation">
-  <div class="nav-previous"><?php next_posts_link( __( '<span class="posts-nav">&larr;</span> Older posts', 'spullen' ) ); ?></div>
-  <div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="posts-nav">&rarr;</span>', 'spullen' ) ); ?></div>
-</div>
-<?php endif; ?>
-
-<?php while ( have_posts() ) : the_post(); ?>
-  <div class="post" id="post-<?php the_ID(); ?>">
-    
+<?php if ( have_posts() ) : ?>
+  <div class="posts-navigation">
+    <div class="nav-previous"><?php next_posts_link( __( '<span class="posts-nav">&larr;</span> Older posts', 'spullen' ) ); ?></div>
+    <div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="posts-nav">&rarr;</span>', 'spullen' ) ); ?></div>
   </div>
-<?php endwhile; ?>
+  
+  <?php while ( have_posts() ) : the_post(); ?>
+    <div class="post" id="post-<?php the_ID(); ?>">
+      
+    </div>
+  <?php endwhile; ?>
+  
+  <div class="posts-navigation">
+    <div class="nav-previous"><?php next_posts_link( __( '<span class="posts-nav">&larr;</span> Older posts', 'spullen' ) ); ?></div>
+    <div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="posts-nav">&rarr;</span>', 'spullen' ) ); ?></div>
+  </div>
+<?php else: ?>
+<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+<?php endif; ?>
 
 <?php if ( $wp_query->max_num_pages > 1 ) : ?>
 <div class="posts-navigation">
